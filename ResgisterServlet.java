e329542c915d6d1db77259077bc685b5f2a1b336
balajisuresh7 <110101166+balajisuresh7@users.noreply.github.com>
2023-01-22 21:43:08 +0530

    Add files via upload

--- ResgisterServlet.java
+++ ResgisterServlet.java
@@ -18,32 +18,14 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class ResgisterServlet
  */
 @WebServlet("/reg")
-//		urlPatterns = { "/reg" }, 
-//		initParams = { 
-//				@WebInitParam(name = "url", value = "jdbc:db2://DSNTDRDA:446/DSNT"), 
-//				@WebInitParam(name = "username", value = "b0s0d1r"), 
-//				@WebInitParam(name = "password", value = "WALMART1")
-//		})
+
 public class ResgisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-//	private Connection connection = null;
+
 
     public void init()
     {
-//    	ServletConfig config = getServletConfig();
-//		String url = config.getInitParameter("url");
-//		String user = config.getInitParameter("username");
-//		String password = config.getInitParameter("password");
-//
-//		try {
-//			Class.forName("com.ibm.db2.jcc.DB2Driver");
-//			connection = DriverManager.getConnection(url, user, password);
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//		}
+
     }
 	
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
@@ -76,33 +58,10 @@ public class ResgisterServlet extends HttpServlet {
 		{	
 			response.sendError(504, "Passwords doesnt match. Please try again");
 		}
-//		PreparedStatement pstmt = null;
-//		String sqlInsertQuery = "insert into b0s0d1r.student "
-//				+ "(STUDENT_ID,STUDENT_FNAME,STUDENT_LNAME,STUDENT_COURSE,STUDENT_DEPT,STUDENT_YEAR,STUDENT_EMAIL,STUDENT_PWD) values(?,?,?,?,?,?,?,?)";
-//		
+		
 		int rowCount = qr.registerNewUser();
 		
-//		try {
-//			if (connection != null)
-//				pstmt = connection.prepareStatement(sqlInsertQuery);
-//			if (pstmt != null) {
-//				pstmt.setInt(1, sid);
-//				pstmt.setString(2,fname);
-//				pstmt.setString(3,lname);
-//				pstmt.setString(4,scourse);
-//				pstmt.setString(5,sdept);
-//				pstmt.setString(6,syear);
-//				pstmt.setString(7,semail);
-//				pstmt.setString(8,spwd);
-//
-//				rowCount = pstmt.executeUpdate();
-//			}
-//
-//		} catch (SQLException se) {
-//			se.printStackTrace();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+
 
 		// 3. Prepare a response and send it to the end user
 		response.setContentType("text/html");
